78f1b34f26eb99dddefcea0dd059a696dbfa7afd
marko <marko@hungry.dev>
2020-09-19 13:48:42 +0200

    Initial Commit

---
+++ Infected/InfectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfectedApp.swift
+//  Infected
+//
+//  Created by marko on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct InfectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
